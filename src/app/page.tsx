ae34ba7b0caa44faf346dbd04ad5def79d4be441
katu <93katu@gmail.com>
2023-06-17 21:15:14 -0300

    listo  pisto 3d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from 'next/image'
+"use client";
+
+import { OrbitControls, Stage } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Piston from "@/3Dcomponents/Piston";
 
 export default function Home() {
   return (
-    <main className="">
-      content
+    <main className="flex h-full items-center justify-center flex-1">
+      <div className="flex-1">Piston</div>
+      <div className="h-[500px] flex-1">
+        <Canvas>
+          <Stage environment="studio" intensity={0.3}>
+            <Piston />
+          </Stage>
+          <OrbitControls enableZoom={false} autoRotate />
+        </Canvas>
+      </div>
     </main>
-  )
+  );
 }

8c251b40ff654548df4b4759721b1a7339258a18
katu <93katu@gmail.com>
2023-06-19 23:28:37 -0300

    arreglada función para resaltar colores

--- src/app/FrontSprocket/page.tsx
+++ src/app/FrontSprocket/page.tsx
@@ -6,8 +6,9 @@ const FrontSprocket = () => {
   const [isHovered, setIsHovered] = useState(false);
   const [hoverClass, setHoverClass] = useState("");
 
-  const handleHover = (e: React.MouseEvent) => {
-    setHoverClass("hovered");
+  const handleHover = (e: React.MouseEvent<SVGPathElement, MouseEvent>) => {
+    const { classList } = e.target as HTMLElement;
+    setHoverClass(classList[0]);
   };
 
   const handleMouseLeave = (e: React.MouseEvent) => {
@@ -17,16 +18,30 @@ const FrontSprocket = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="w-[500px]">
-        <FrontSprocketSVG handleHover={handleHover} handleMouseLeave={handleMouseLeave} hoverClass={hoverClass}/>
+        <FrontSprocketSVG
+          handleHover={handleHover}
+          handleMouseLeave={handleMouseLeave}
+          hoverClass={hoverClass}
+          hoveredClass={hoverClass}
+        />
       </div>
       <div>
         <h1
-          className={`sizeA ${hoverClass}`}
+          className={`sizeA ${
+            hoverClass === "sizeA" ? "text-[#3bc83b]" : "text-[#0c950c]"
+          } `}
           onMouseEnter={handleHover}
           onMouseLeave={handleMouseLeave}
-          // className={'sizeA'}
         >
-          AAA
+          Medida A
+        </h1>
+
+        <h1
+          className={`sizeB ${hoverClass === "sizeB" ? "#yrllo" : "#0c950c"} `}
+          onMouseEnter={handleHover}
+          onMouseLeave={handleMouseLeave}
+        >
+          Medida B
         </h1>
       </div>
     </div>

2ba332d37a671bdaa5906ef52cf0585b3d8b79b3
katu <93katu@gmail.com>
2023-06-19 21:23:14 -0300

    funcion para resaltar parte especifica creadas

--- src/app/FrontSprocket/page.tsx
+++ src/app/FrontSprocket/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+import React, { useRef, useState } from "react";
+import "./frontSprocketStyle.css";
+import FrontSprocketSVG from "../../../public/svgParts/FrontSprocket";
 const FrontSprocket = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [hoverClass, setHoverClass] = useState("");
+
+  const handleHover = (e: React.MouseEvent) => {
+    setHoverClass("hovered");
+  };
+
+  const handleMouseLeave = (e: React.MouseEvent) => {
+    setHoverClass("");
+  };
+
   return (
-    <div>FrontSprocket</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center">
+      <div className="w-[500px]">
+        <FrontSprocketSVG handleHover={handleHover} handleMouseLeave={handleMouseLeave} hoverClass={hoverClass}/>
+      </div>
+      <div>
+        <h1
+          className={`sizeA ${hoverClass}`}
+          onMouseEnter={handleHover}
+          onMouseLeave={handleMouseLeave}
+          // className={'sizeA'}
+        >
+          AAA
+        </h1>
+      </div>
+    </div>
+  );
+};
 
 export default FrontSprocket;

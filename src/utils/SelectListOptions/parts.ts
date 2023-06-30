a556bbdaeaf49618d57bb8a424a48bf0d88d0a98
katu <93katu@gmail.com>
2023-06-29 21:22:18 -0300

    cambio de posision lista de partes

--- src/utils/SelectListOptions/parts.ts
+++ src/utils/SelectListOptions/parts.ts
@@ -2,11 +2,11 @@ import { possibleParts } from "@/types-enums-interfaces/partEnum";
 
 export const partsOptions = [
   {
-    displayName: 'Front Sprocket (large spline)',
-    value: possibleParts.FSLargeSpline,
+    displayName: "Front Sprocket (narrow spline)",
+    value: possibleParts.FSNarrowSpline,
   },
   {
-    displayName: 'Front Sprocket (narrow spline)',
-    value: possibleParts.FSNarrowSpline,
+    displayName: "Front Sprocket (large spline)",
+    value: possibleParts.FSLargeSpline,
   },
 ];

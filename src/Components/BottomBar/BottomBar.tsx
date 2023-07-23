5ce589b1de2c108a43f7154bb1605a38b6e07a09
katu <93katu@gmail.com>
2023-07-23 16:39:15 -0300

    Agregados links de respuestos a footer

--- src/Components/BottomBar/BottomBar.tsx
+++ src/Components/BottomBar/BottomBar.tsx
@@ -1,15 +1,23 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 const BottomBar = () => {
-  const pathname = usePathname();
   return (
-    <footer className="flex justify-center bg-black px-4 py-2 text-white">
+    <footer className="flex flex-col items-center justify-center laptop:flex-row laptop:gap-16 laptop:text-left bg-black px-4 py-2 text-white text-center">
       <div className="flex flex-col gap-2">
         <Link href="/PrivacyPolicy">Privacy Policy</Link>
         <Link href="/TermsAndConditions">Terms & Conditions</Link>
         <Link href="/Contact">Contact</Link>
+      </div>
+      <div className="flex flex-col gap-2">
+        <Link href="/BrakeDisc">Brake Discs</Link>
+        <Link href="/ConnectingRods">Connecting Rods</Link>
+        <Link href="/FrontSprocket">Front Sprockets</Link>
+      </div>
+      <div className="flex flex-col gap-2">
+        <Link href="/RearSprocket">Rear Sprockets</Link>
+        <Link href="/PistonKit">Piston Kit</Link>
+        <Link href="/Valve">Valves</Link>
       </div>
     </footer>
   );

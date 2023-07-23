6a5dab6f94654c76e9884cd2899493c0f69a9ccc
katu <93katu@gmail.com>
2023-07-23 16:26:23 -0300

    formulario basico no funcional de contacto

--- src/Components/BottomBar/BottomBar.tsx
+++ src/Components/BottomBar/BottomBar.tsx
@@ -4,12 +4,13 @@ import { usePathname } from "next/navigation";
 
 const BottomBar = () => {
   const pathname = usePathname();
-;
-
   return (
-    <footer className="flex h-[80px] justify-center bg-black text-white py-2 px-4">
-      <Link href='/PrivacyPolicy'>Privacy Policy</Link>
-      <Link href='/TermsAndConditions'>Terms & Conditions</Link>
+    <footer className="flex justify-center bg-black px-4 py-2 text-white">
+      <div className="flex flex-col gap-2">
+        <Link href="/PrivacyPolicy">Privacy Policy</Link>
+        <Link href="/TermsAndConditions">Terms & Conditions</Link>
+        <Link href="/Contact">Contact</Link>
+      </div>
     </footer>
   );
 };

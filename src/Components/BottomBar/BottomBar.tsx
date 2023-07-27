835afa3c7faab46d7f5e7bb704853a0f84bf9f85
katu <93katu@gmail.com>
2023-07-27 18:18:16 -0300

    pagina y boton de donacion configurados

--- src/Components/BottomBar/BottomBar.tsx
+++ src/Components/BottomBar/BottomBar.tsx
@@ -1,14 +1,11 @@
 "use client";
+import Image from "next/image";
 import Link from "next/link";
 
 const BottomBar = () => {
   return (
-    <footer className="flex flex-col items-center justify-center laptop:flex-row laptop:gap-16 laptop:text-left bg-black px-4 py-2 text-white text-center">
-      <div className="flex flex-col laptop:gap-2">
-        <Link href="/PrivacyPolicy">Privacy Policy</Link>
-        <Link href="/TermsAndConditions">Terms & Conditions</Link>
-        <Link href="/Contact">Contact</Link>
-      </div>
+    <footer className="flex flex-col items-center justify-center bg-black  p-4 text-center text-white laptop:flex-row laptop:items-start laptop:gap-16 laptop:text-left">
+      
       <div className="flex flex-col laptop:gap-2">
         <Link href="/BrakeDisc">Brake Discs</Link>
         <Link href="/ConnectingRods">Connecting Rods</Link>
@@ -18,6 +15,20 @@ const BottomBar = () => {
         <Link href="/RearSprocket">Rear Sprockets</Link>
         <Link href="/PistonKit">Piston Kit</Link>
         <Link href="/Valve">Valves</Link>
+      </div>
+      <div className="flex flex-col laptop:gap-2">
+        <Link href="/PrivacyPolicy">Privacy Policy</Link>
+        <Link href="/TermsAndConditions">Terms & Conditions</Link>
+        <Link href="/Contact">Contact</Link>
+        <Link href="/Donate" className="text-center">
+          <Image
+            src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+            alt="Donation buttom"
+            width={70}
+            height={30}
+            className="mx-auto laptop:m-0"
+          />
+        </Link>
       </div>
     </footer>
   );
